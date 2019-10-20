c9000954f3e9ec1ae8f00491f515fda9afc3165f
sreeraj <sreerajavk123@gmail.com>
2019-10-20 21:12:05 +0530

    Updates

---
+++ precache-manifest.d2fa26801c2b9b3e8ca1b55fd603719c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d98e554f7408f465583d3e3c5a1e61",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "6b1f31bdcc3dd816a795",
+    "url": "/test/static/js/2.22251a5a.chunk.js"
+  },
+  {
+    "revision": "ddaefe34abdddc6512d1",
+    "url": "/test/static/js/main.802e5b84.chunk.js"
+  },
+  {
+    "revision": "138984615eff7659bf16",
+    "url": "/test/static/js/runtime-main.3ea88747.js"
+  },
+  {
+    "revision": "dcf2add06d5af43cda68863ddadc7397",
+    "url": "/test/static/media/banner.dcf2add0.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/test/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test/static/media/logo.25bf045c.svg"
+  }
+]);
